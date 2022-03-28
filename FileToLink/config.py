0f68e713a0c78b9b89a1d374d03110beaea6cb8c
ElJoker63 <eljoker630@gmail.com>
2022-03-28 05:40:03 -0400

    .

--- FileToLink/config.py
+++ FileToLink/config.py
@@ -12,13 +12,18 @@ class Config:
     Port = int(os.environ.get("PORT"))
     Archive_Channel_ID = int(os.environ.get("ARCHIVE_CHANNEL_ID"))
     Start_Message = os.environ.get("Start_Message")
+    Bot_Channel = os.environ.get("Bot_Channel_UserName")
+    if Bot_Channel and Bot_Channel.startswith("@"):
+        Bot_Channel = Bot_Channel[1:]
+    elif Bot_Channel == "":
+        Bot_Channel = None
 
     Link_Root = f"https://{App_Name}.herokuapp.com/"
     Download_Folder = "Files"
-    Bot_Channel = "thys3"
+    Dev_Channel = "ElJoker63"
     Bot_UserName = None  # The bot will set it after starting
     Part_size = 10 * 1024 * 1024  # (10MB) For Pyrogram
-    Buffer_Size = 256 * 1024  # For Quart
+    Buffer_Size = 512 * 1024  # For Quart
     Pre_Dl = 3  # How many parts to download from telegram before client request them
     Separate_Time = 4  # (seconds)  wait time between messages if user send more than one
     Sleep_Threshold = 60  # (Seconds) sleep threshold for flood wait exceptions
@@ -26,15 +31,12 @@ class Config:
 
 
 class Strings:
-    start = """⭐️ Hola , Envíame cualquier archivo para obtener el enlace de descarga directa
-
-⁉️ Enviar /help para obtener más información
-
-💬 Enviar /lang para cambiar el idioma del bot"""
+    start = Config.Start_Message
     dl_link = "🔗 Download LINK"
     st_link = "🎞 Stream LINK"
     generating_link = "**⏳ Generating Link...**"
-    join_channel = "📢 Bot Channel"
+    bot_channel = "📢 Bot Channel"
+    dev_channel = "🤖 Developer"
     fast = "⚡️**The link has been updated to a fast link**"
     update_link = "⚡ Update To Fast Link"
     update_limited = (f"⛔ You can update just {Config.Max_Fast_Processes} link in one time, "
@@ -47,3 +49,4 @@ class Strings:
     file_not_found = "⚠️File Not Found, Please resend it again"
     delete_manually_button = "⚠️You can delete it"
     delete_forbidden = "The bot can't delete messages older than 48 hours, you can delete this message manually"
+    force_join = "⚠ Join Bot Channel to use this Bot"
