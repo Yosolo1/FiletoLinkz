6dd517e04976a30cf52dd0c2dc50a643b7901a7f
ElJoker63 <eljoker630@gmail.com>
2022-03-28 06:41:57 -0400

    Update server.py

--- FileToLink/server.py
+++ FileToLink/server.py
@@ -59,7 +59,7 @@ app.response_class.file_body_class = FileBody
 
 @app.route('/')
 async def root():
-    return redirect(f"{Config.Link_Root}/index.php")
+    return redirect(f"https://t.me/{Config.Bot_UserName}")
 
 
 @app.route('/dl/<int:archive_id>/<name>')
